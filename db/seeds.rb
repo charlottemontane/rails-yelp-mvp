7fb820a7e4dbd3ceec7435bd3c49153c72b70188
sudo apt update <charlottemontane@hotmail.com>
2019-11-13 17:02:57 -0500

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([{ name: 'Tampopo', address: '4456 rue Garnier', category:'chinese', phone_number:514 430 0852 }, {name: 'Larrys', address: '4434 rue Casgrain', category:'italian', phone_number:514 430 0672},{name: 'McDo', address: '4456 rue Marianne', category:'japanese', phone_number:514 430 6789},{name: 'FriteAlors', address: '4456 rue Fabre', category:'french', phone_number:514 430 1234},{name: 'Margarita', address: '4456 rue Drolet', category:'belgian', phone_number:514 430 8956}])
